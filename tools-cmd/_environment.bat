42e66a99e9a79c701bef53441d4ea190ce495147
Jim Derry <balthisar@gmail.com>
2016-02-22 15:38:58 +0800

    Add new scheme to Windows, too.

--- tools-cmd/_environment.bat
+++ tools-cmd/_environment.bat
@@ -29,23 +29,19 @@ rem ###########################################################################
 
     rem # These are all relative from the TY_PROJECT_ROOT_DIR directory.
     set TY_CASES_BASE_DIR=cases
-    set TY_RESULTS_DIR=results
+    set TY_RESULTS_BASE_DIR=results
 
     rem # These are relative to the TY_CASES_BASE_DIR directory.
-    set TY_CASES_DIR_tmp=cases-testbase
-    set TY_LOG_FILE=results-log.txt
-    set TY_RESULTS_FILE=results-results.txt
     set TY_TMP_DIR=tmp
-    set TY_TMP_FILE=results-temp.txt
 
     rem # These are expected to be in cases_base_dir directory.
-    set TY_EXPECTS_TESTBASE=expects-testbase.txt
-    set TY_EXPECTS_ACCESS=expects-accesscases.txt
-    set TY_EXPECTS_XML=expects-xmlcases.txt
     set TY_VERSION_FILE=version.txt
 
     rem # This file must exist in any test cases directory.
     set TY_CONFIG_DEFAULT=config_default.conf
+    
+    rem # The default cases set name
+    set cases_setname=testbase
 
 GOTO:EOF
 
@@ -56,17 +52,16 @@ rem ###########################################################################
 
     set TY_PROJECT_ROOT_DIR=
     set TY_CASES_BASE_DIR=
-    set TY_RESULTS_DIR=
     set TY_CASES_DIR=
-    set TY_LOG_FILE=
+    set TY_EXPECTS_DIR=
+    set TY_EXPECTS_FILE=
+    set TY_CONFIG_DEFAULT=
+    set TY_VERSION_FILE=
+    set TY_RESULTS_BASE_DIR=
+    set TY_RESULTS_DIR=
     set TY_RESULTS_FILE=
     set TY_TMP_DIR=
     set TY_TMP_FILE=
-    set TY_EXPECTS_TESTBASE=
-    set TY_EXPECTS_ACCESS=
-    set TY_EXPECTS_XML=
-    set TY_VERSION_FILE=
-    set TY_CONFIG_DEFAULT=
     
 GOTO:EOF
 
@@ -82,25 +77,24 @@ rem ###########################################################################
     pushd %TY_PROJECT_ROOT_DIR%
     set TY_PROJECT_ROOT_DIR=%CD%
     popd
+    
+    rem # *Only* set TY_CASES_SETNAME if it's not already set!
+    IF NOT DEFINED TY_CASES_SETNAME (set TY_CASES_SETNAME=%cases_setname%)
 
     set TY_PROJECT_ROOT_DIR=%TY_PROJECT_ROOT_DIR%
     set TY_CASES_BASE_DIR=%TY_PROJECT_ROOT_DIR%\%TY_CASES_BASE_DIR%
-    set TY_RESULTS_DIR=%TY_PROJECT_ROOT_DIR%\%TY_RESULTS_DIR%
-
-    rem # *Only* set TY_CASES_DIR if it's not already set!
-    IF NOT DEFINED TY_CASES_DIR (set TY_CASES_DIR=%TY_CASES_BASE_DIR%\%TY_CASES_DIR_tmp%)
-
-    set TY_LOG_FILE=%TY_RESULTS_DIR%\%TY_LOG_FILE%
-    set TY_RESULTS_FILE=%TY_RESULTS_DIR%\%TY_RESULTS_FILE%
-    set TY_TMP_DIR=%TY_RESULTS_DIR%\%TY_TMP_DIR%
-    set TY_TMP_FILE=%TY_RESULTS_DIR%\%TY_TEMP_DIR%\%TY_TMP_FILE%
-    set TY_EXPECTS_TESTBASE=%TY_CASES_BASE_DIR%\%TY_EXPECTS_TESTBASE%
-    set TY_EXPECTS_ACCESS=%TY_CASES_BASE_DIR%\%TY_EXPECTS_ACCESS%
-    set TY_EXPECTS_XML=%TY_CASES_BASE_DIR%\%TY_EXPECTS_XML%
-    set TY_VERSION_FILE=%TY_CASES_BASE_DIR%\%TY_VERSION_FILE%
+    set TY_CASES_DIR=%TY_CASES_BASE_DIR%\cases-%TY_CASES_SETNAME%
+    set TY_EXPECTS_DIR=%TY_CASES_BASE_DIR%\cases-%TY_CASES_SETNAME%-expects
+    set TY_EXPECTS_FILE=%TY_CASES_BASE_DIR%\cases-%TY_CASES_SETNAME%-expects.txt
     set TY_CONFIG_DEFAULT=%TY_CASES_DIR%\%TY_CONFIG_DEFAULT%
+    set TY_VERSION_FILE=%TY_CASES_BASE_DIR%\%TY_VERSION_FILE%
+    set TY_RESULTS_BASE_DIR=%TY_PROJECT_ROOT_DIR%\%TY_RESULTS_BASE_DIR%
+    set TY_RESULTS_DIR=%TY_RESULTS_BASE_DIR%\cases-%TY_CASES_SETNAME%-results
+    set TY_RESULTS_FILE=%TY_RESULTS_BASE_DIR%\cases-%TY_CASES_SETNAME%-results.txt
+    set TY_TMP_DIR=%TY_RESULTS_BASE_DIR%\%TY_TMP_DIR%
+    set TY_TMP_FILE=%TY_TMP_DIR%\temp.txt
 
-    set TY_CASES_DIR_tmp=
+    set cases_setname=
 
 GOTO:EOF
 
@@ -111,18 +105,16 @@ rem ###########################################################################
 
     echo TY_PROJECT_ROOT_DIR = %TY_PROJECT_ROOT_DIR%
     echo   TY_CASES_BASE_DIR = %TY_CASES_BASE_DIR%
-    echo      TY_RESULTS_DIR = %TY_RESULTS_DIR%
     echo        TY_CASES_DIR = %TY_CASES_DIR%
-    echo         TY_LOG_FILE = %TY_LOG_FILE%
+    echo      TY_EXPECTS_DIR = %TY_EXPECTS_DIR%
+    echo     TY_EXPECTS_FILE = %TY_EXPECTS_FILE%
+    echo   TY_CONFIG_DEFAULT = %TY_CONFIG_DEFAULT%
+    echo     TY_VERSION_FILE = %TY_VERSION_FILE%
+    echo TY_RESULTS_BASE_DIR = %TY_RESULTS_BASE_DIR%
+    echo      TY_RESULTS_DIR = %TY_RESULTS_DIR%
     echo     TY_RESULTS_FILE = %TY_RESULTS_FILE%
     echo          TY_TMP_DIR = %TY_TMP_DIR%
     echo         TY_TMP_FILE = %TY_TMP_FILE%
-    echo TY_EXPECTS_TESTBASE = %TY_EXPECTS_TESTBASE%
-    echo   TY_EXPECTS_ACCESS = %TY_EXPECTS_ACCESS%
-    echo      TY_EXPECTS_XML = %TY_EXPECTS_XML%
-    echo     TY_VERSION_FILE = %TY_VERSION_FILE%
-    echo   TY_CONFIG_DEFAULT = %TY_CONFIG_DEFAULT%
-    echo        TY_TIDY_PATH = %TY_TIDY_PATH%
 
 GOTO:EOF
 
