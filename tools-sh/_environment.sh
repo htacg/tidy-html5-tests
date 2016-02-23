d22870387b8acac06f105ab76347338756abb098
Jim Derry <balthisar@gmail.com>
2016-02-23 09:15:19 +0800

    Lose the cases- prefix.

--- tools-sh/_environment.sh
+++ tools-sh/_environment.sh
@@ -78,8 +78,8 @@ set_environment()
     
     export TY_PROJECT_ROOT_DIR="${project_root_dir}"
     export TY_CASES_BASE_DIR="${TY_PROJECT_ROOT_DIR}/${cases_base_dir}"
-    export TY_CASES_DIR="${TY_CASES_BASE_DIR}/cases-${TY_CASES_SETNAME}"
-    export TY_EXPECTS_DIR="${TY_CASES_BASE_DIR}/cases-${TY_CASES_SETNAME}-expects"
+    export TY_CASES_DIR="${TY_CASES_BASE_DIR}/${TY_CASES_SETNAME}"
+    export TY_EXPECTS_DIR="${TY_CASES_BASE_DIR}/${TY_CASES_SETNAME}-expects"
     export TY_EXPECTS_FILE="${TY_CASES_DIR}/_manifest.txt"
     export TY_CONFIG_DEFAULT="${TY_CASES_DIR}/${config_default}"
     export TY_VERSION_FILE="${TY_CASES_BASE_DIR}/${version_file}"
