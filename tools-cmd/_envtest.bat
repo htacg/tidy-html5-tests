a7957448e013d0d7f157fb8c9e3a704d32899bf6
Jim Derry <balthisar@gmail.com>
2016-02-25 21:40:49 +0800

    Cleanup test with setlocal assumption.

--- tools-cmd/_envtest.bat
+++ tools-cmd/_envtest.bat
@@ -1,20 +1,16 @@
+@setlocal
 @echo off
 
-rem # Allow user to specify a different Tidy. Do this before any setlocal!
+rem # Allow user to specify a different Tidy.
 IF NOT "%~1" == "" (
     echo setting TY_TIDY_PATH
     set TY_TIDY_PATH="%~1"
 )
 
-rem # Do this before any type of setlocal!
 call _environment.bat :set_environment
 
-rem # Now we can do setlocal and do normal script stuff.
-setlocal
-
-rem # See that our env vars are still set.
 call _environment.bat :report_environment
 
-rem # Let's unset our env for a clean exit.
 endlocal
-call _environment.bat :unset_environment
+
+call _environment.bat :report_environment
